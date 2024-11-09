85a0fdfcc845b0cca0d86a9460c8fb205460b6bb
Ben Chih-Cheng Chien <68105128+aliveben2k@users.noreply.github.com>
2024-11-09 12:20:08 +0900

    Add files via upload

--- distance_density/distance_density_adzuki.R
+++ distance_density/distance_density_adzuki.R
@@ -17,7 +17,8 @@ modes <- function(d){
 
 #col.brewer.theme <- c("dodgerblue","orange","red","navy", "red4", "gold", "darkviolet", "maroon1", "peru", "coral1") #user_defined
 #col.brewer.theme <- c("black","grey50","grey75","dodgerblue", "orange", "red", "darkviolet", "maroon1", "peru", "coral1") #user_defined
-col.brewer.theme <- c("black","grey50","grey75","#4b8bcb","gold","#ed3325","#255271", "#f7931e", "#921a1d", "#7758a5", "#f9bbb9", "#c6b1d4", "#ed7f6d", "#90a7b7")
+#col.brewer.theme <- c("black","grey50","grey75","#4b8bcb","gold","#ed3325","#255271", "#f7931e", "#921a1d", "#7758a5", "#f9bbb9", "#c6b1d4", "#ed7f6d", "#90a7b7")
+col.brewer.theme <- c("#255271","#4b8bcb","gold","#f9bbb9", "#c6b1d4", "#90a7b7")
 
 args <- commandArgs(trailingOnly = TRUE)
 matrix <- read.table(args[1], sep = "\t")
@@ -39,16 +40,15 @@ final.table <- c()
 y.max <- c()
 for(i in 1:length(uni.group)){
   for (k in i:length(uni.group)){
-    ##original
-    #p1.subset <- matrix[which(matrix$group == uni.group[i]),]
-    #p1.subset.mean <- colMeans(p1.subset[,3:ncol(p1.subset)])
-    #message(uni.group[k])
-    #p2.idx <- grep(uni.group[k], matrix$group)
-    #p1.vs.p2.dist <- p1.subset.mean[p2.idx]
-    #message(paste0(uni.group[i], " vs. ",uni.group[k]))
-    ##CRLee
     p1.vs.p2.dist <- as.matrix(matrix[which(group.info[,2] == uni.group[i]),which(group.info[,2] == uni.group[k])])
-    d <- density(as.numeric(p1.vs.p2.dist[upper.tri(p1.vs.p2.dist)]))
+    if (uni.group[i] == uni.group[k]){
+      p1.vs.p2.dist[lower.tri(p1.vs.p2.dist)] <-NA
+      p1.vs.p2.dist[p1.vs.p2.dist == 0] <- NA
+      
+    }
+    p1.vs.p2.dist <- as.vector(p1.vs.p2.dist)
+    p1.vs.p2.dist <- p1.vs.p2.dist[!is.na(p1.vs.p2.dist)]
+    d <- density(p1.vs.p2.dist)
     d <- data.frame(x = d$x, y = d$y)
     y.max <- c(y.max, max(d$y))
     if (uni.group[i] == uni.group[k]){
@@ -68,6 +68,7 @@ y.max <- max(y.max)
 
 col.brewer.theme <- c("#255271","#4b8bcb","gold","#f9bbb9", "#c6b1d4", "#90a7b7")
 final.table$group <- factor(final.table$group, levels = c("JP_WL","JP_LR","CN_LRN","JP_LR vs. JP_WL","JP_WL vs. CN_LRN","JP_LR vs. CN_LRN"))
+#write.table(final.table, file = "/Users/ben/Library/CloudStorage/GoogleDrive-aliveben@gmail.com/Other\ computers/Ben_Home_PC/cloud_files/Lee\ lab/Vigna\ angularis/new_gatk4/final_no_out/20211222_0.7/m_dist_density_paper.txt", quote = F, sep = "\t", col.names = T, row.names = T)
 
 out.plot <- ggplot(data.frame(final.table), aes(x = as.numeric(x), y = as.numeric(y), group = group)) +
   #stat_density(aes(x=as.numeric(x)), geom="line", position="identity", size = 1) +
@@ -75,7 +76,7 @@ out.plot <- ggplot(data.frame(final.table), aes(x = as.numeric(x), y = as.numeri
   geom_area(position="identity", aes(fill = group), size = NULL, alpha = 0.8) +
   scale_color_manual(values = col.pal) +
   scale_fill_manual(values = col.pal, breaks = c("JP_WL","JP_LR","CN_LRN","JP_LR vs. JP_WL","JP_WL vs. CN_LRN","JP_LR vs. CN_LRN")) +
-  scale_x_continuous(breaks = seq(0.000, 0.025, by = 0.01), name = "Pairwise distance") +
+  scale_x_continuous(breaks = seq(0.000, 0.025, by = 0.010), name = "Pairwise distance") +
   ylab("Density") +
   theme_minimal() +
   theme(text = element_text(color = "black", size = 7.5),
@@ -86,15 +87,18 @@ out.plot <- ggplot(data.frame(final.table), aes(x = as.numeric(x), y = as.numeri
         axis.text = element_text(color = "black", size = 7.5),
         axis.title = element_text(color = "black", size = 7.5),
         legend.position = "none",
+        legend.key.size = unit(0.3, 'cm'),
+        legend.text = element_text(color = "black", size = 7.5),
         aspect.ratio = 1)
   #xlim(0, x.max)# + ylim(0,y.max*1.3)
   
 
-pdf(paste0(args[3],"/distance_density.pdf"))
-print(out.plot)
-dev.off()
+#pdf(paste0(args[3],"/distance_density.pdf"))
+#print(out.plot)
+#dev.off()
 
-name_a <- "E:\\cloud_files\\Lee\ lab\\Vigna\ angularis\\new_gatk4\\final_no_out\\20211222_0.7\\distance_density_north.tiff"
+#name_a <- "E:\\cloud_files\\Lee\ lab\\Vigna\ angularis\\new_gatk4\\final_no_out\\20211222_0.7\\distance_density_north.tiff"
+name_a <- "/Users/ben/Library/CloudStorage/GoogleDrive-aliveben@gmail.com/Other\ computers/Ben_Home_PC/cloud_files/Lee\ lab/Vigna\ angularis/new_gatk4/final_no_out/20211222_0.7/distance_density_north.tiff"
 tiff(name_a, units="cm", width = 4, height = 4, res = 600)
 print(out.plot)
 dev.off()
